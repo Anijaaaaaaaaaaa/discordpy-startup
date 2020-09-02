04d0bc6b098fb031450396eeba560b7e671d38e2
The.First.Step <46297678+Anijaaaaaaaaaaa@users.noreply.github.com>
2020-09-02 22:05:30 +0900

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -21,7 +21,7 @@ class helps(commands.Cog):
             discord = {"公式鯖の招待": "https://discord.gg/d7Qqfhy", "ログ鯖の招待": "https://discord.gg/VHuzWKv", "絵師鯖の招待": "https://discord.gg/qdVsWjE"}
 
             des = "```現在、諸事情により9月の16日まで使用不可能となっております。\nTAOの今後は公式鯖に入っていればお知らせ随時お知らせ致します。\n下記のURLにTAO関連のURLを張り付けております。```\n"
-            des += "> TAO自体の寄付金は止めています。\n諸事情を知って個人的に寄付をしてくださる方は\n<@304932786286886912>のDMまでお願いします；；"
+            des += ">>> TAO自体の寄付金は止めています。\n諸事情を知って個人的に寄付をしてくださる方は\n<@304932786286886912>のDMまでお願いします；；"
 
             embed = Embed(title=f"現在TAOは使用できません。", description=des)
             embed.add_field(name="BOT招待:", value=" | ".join([f"[{t[0]}](<{t[1]}>)" for t in list(invite.items())]), inline=False)
